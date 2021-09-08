7d37aa16b165d75e2da55207f300704b54e22ad0
sdell <>
2021-09-07 23:57:38 -0400

    Rework hexview to an HTML view. Still trying to figure out how to add icon

---
+++ src/scripts/hexviewHtml.js
@@ -0,0 +1,60 @@
+/** 
+ * NOTES:
+ *  JavaScript updates view faster than TypeScript.
+ *  onClick, would not work properly in TypeScript.
+ *  Non formatted hex is stored in hidden element of the HTML for easy updating.
+**/
+
+// Function to update the yellow highlighted text based on the item clicked on
+function updateSelection(elementId) {
+    updateHtml(-1, -1)
+    const element = /** @type {HTMLElement} */ (document.getElementById(elementId));
+    element.style.color = element.style.color === "yellow" ? "white" : "yellow";
+}
+
+// Function for creating the html for the hex
+function getHexHtml(lines, lineNum, dataPositon) {
+    let htmlLines = "";
+    let lineCount = 0;
+    lines = lines.split('\n');
+
+    lines.forEach((line) => {
+        htmlLines += "<p>";
+
+        let lineParts = line.split(" ");
+
+        for (var i=0; i < lineParts.length; i++) {
+            let spanId = `byteSpan-${lineCount}-${i}`;
+            let textColor = line === lines[lineNum] && i === dataPositon && lineParts[i] === lineParts[dataPositon] ? "yellow" : "white";
+            htmlLines += `<span id="${spanId}" onclick="updateSelection('${spanId}');" style="color: ${textColor};">${lineParts[i]}</span> `;
+        }
+    
+        htmlLines += "</p>";
+        lineCount += 1;
+    });
+
+    return htmlLines;
+}
+
+// Function that will update the html of the hex web view
+function updateHtml(lineNum, dataPosition) {
+    const nonHtmlHexObj = /** @type {HTMLElement} */ (document.getElementById('nonHtmlHex'));
+    const hexHtmlObj = /** @type {HTMLElement} */ (document.getElementById('hexHtml'));
+    var lines = nonHtmlHexObj.textContent;
+    var htmlLines = getHexHtml(lines, lineNum, dataPosition)
+    hexHtmlObj.innerHTML = htmlLines;
+}
+
+// Function that gets called by default to create and update the hex web view
+(function main() {
+    const vscode = acquireVsCodeApi();
+
+    var lineNum = 0;
+    var dataPosition = 1;
+    updateHtml(lineNum, dataPosition)
+
+    window.addEventListener('message', event => {
+        const message = event.data;
+        updateHtml(message.args.lineNum, message.args.dataPosition);
+    });
+} ());
